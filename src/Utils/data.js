752eabf189aed9c2c68d7f607d908c8be7ba7883
DeriFirgiawan <derifirgiawan025@gmail.com>
2021-05-01 14:39:51 +0700

    Delete Data unused

--- src/Utils/data.js
+++
@@ -1,8 +0,0 @@
-export const Data = {
-  content: {
-    about: {
-      title: "Hello Everyone 👋",
-      text: "I'm Deri from Bandung, Indonesian. I am a Web Front End Developer. I enjoy Web Development using React JS and am open to freelance projects."
-    }
-  }
-}
