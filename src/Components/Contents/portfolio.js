fcf76d26057589c0e626848cb4f60c55edd4d3d8
DeriFirgiawan <derifirgiawan025@gmail.com>
2021-05-02 20:35:03 +0700

    Fix style and animation

--- src/Components/Contents/portfolio.js
+++ src/Components/Contents/portfolio.js
@@ -1,8 +1,8 @@
 const Portfolio = ({title, desc, children, image}) => {
   return (
-    <div className="w-60 xs:w-full xs:my-20 my-6 h-screen mb-auto" aria-label={title}>
-      <div className="p-4 bg-gray-200 h-40">
-        <img src={image} className="h-32" alt={title} title={title} />
+    <div className="w-60 xs:w-full xs:my-20 my-6" aria-label={title}>
+      <div className="p-4 bg-gray-200 h-40 xs:p-0">
+        <img src={image} className="h-32 xs:h-auto" alt={title} title={title} />
       </div>
       <div className="my-4" aria-label={desc}>
         <p className="text-base tex-black font-medium" aria-label={title}>{title}</p>
--- src/Components/Contents/repositories.js
+++ src/Components/Contents/repositories.js
@@ -1,6 +1,6 @@
 const Repositories = ({title, desc, link}) => {
   return (
-    <div className="shadow-md border-gray-200 p-5 my-7 w-1/2 rounded h-44 mx-4 my-4 xs:h-auto md:w-full xs:w-full xs:justify-center" aria-label="Repositories"  data-aos="zoom-in" data-aos-offset="600" data-aos-once="true">
+    <div className="shadow-md border-gray-200 p-5 my-7 w-1/2 rounded h-44 mx-4 my-4 xs:h-auto md:w-full xs:w-full xs:justify-center" aria-label="Repositories"  data-aos="zoom-in" data-aos-offset="400" data-aos-once="true">
       <h2 className="text-lg text-gray-500 font-semibold" aria-label={title}>{title}</h2>
       <p className="text-sm text-gray-400 my-2 font-medium" aria-label={desc}>{desc}</p>
       <div className="my-3 flex justify-between items-center" aria-label="Action Link">
