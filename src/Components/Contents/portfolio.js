27a396605fc4ed73aab57fae95e14ae48edc055e
DeriFirgiawan <derifirgiawan025@gmail.com>
2021-05-02 22:01:39 +0700

    Fixed Style

--- src/Components/Contents/portfolio.js
+++ src/Components/Contents/portfolio.js
@@ -1,16 +1,16 @@
 const Portfolio = ({title, desc, children, image}) => {
   return (
-    <div className="w-60 xs:w-full xs:my-20 my-6" aria-label={title}>
+    <div className="w-60 xs:w-full xs:my-20 my-6 h-96" aria-label={title}>
       <div className="p-4 bg-gray-200 h-40 xs:p-0">
         <img src={image} className="h-32 xs:h-auto" alt={title} title={title} />
       </div>
-      <div className="my-4" aria-label={desc}>
+      <div className="my-6 relative xs:top-20" aria-label={desc}>
         <p className="text-base tex-black font-medium" aria-label={title}>{title}</p>
         <p className="text-base text-gray-400 my-4">{desc}</p>
+        <div className="flex flex-col" aria-label="Action Link">
+          {children}
+        </div>
       </div>
-      <div className="flex flex-col relative top-0 bottom-0" aria-label="Action Link">
-        {children}
-      </div> 
     </div> 
   )
 }
