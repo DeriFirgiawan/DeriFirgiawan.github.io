45d9fc9b40e71fca3d49c3a6bf0cf26c150629f9
derifirgiawan_025 <derifirgiawan025@gmail.com>
2021-08-08 00:07:30 +0700

    modified

--- src/Components/Contents/portfolio.js
+++ src/Components/Contents/portfolio.js
@@ -4,7 +4,7 @@ const Portfolio = ({title, desc, children, image}) => {
       <div className="p-4 bg-gray-200 h-40 xs:p-0">
         <img src={image} className="h-32 xs:h-auto" alt={title} title={title} />
       </div>
-      <div className="my-6 relative xs:top-20" aria-label={desc}>
+      <div className="my-6 relative xs:top-20 xs:my-0" aria-label={desc}>
         <p className="text-base tex-black font-medium" aria-label={title}>{title}</p>
         <p className="text-base text-gray-400 my-4">{desc}</p>
         <div className="flex flex-col" aria-label="Action Link">
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -7,7 +7,7 @@ const Header = ({title, text}) => {
       <div className="p-2 my-4">
         <h1 className="text-2xl text-black font-semibold" aria-label={title} title={title}>{title}</h1>
         <p className="text-base text-gray-400 font-medium my-2" aria-label={text}>
-          {text}
+          I'm Deri from Bandung, Indonesia. I am a <span className="font-semibold text-black">Front-End Engineer</span> and a <span className="font-semibold text-black">Freelancer</span>. I really enjoy Mobile App development using <span className="font-semibold text-black">React Native</span> and I'm also very happy with <span className="font-semibold text-black">React JS</span> for Web App development.
         </p>
         <div className="animate-pulse">
           <a href="#contact" rel="noopener noreferrer" className="text-base text-black underline font-medium hover:font-bold my-4" aria-label="Contact Me" title="Contact Me">Contact Me</a>
